66ef5e7d7945251c9ef0229888f9ed28d598bf35
thergh <lukasz.ciswicki@gmail.com>
2024-01-21 21:37:27 +0100

    1.0 updated values

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,9 @@
 #include "Util.h"
 #include <vector>
 #include <iostream>
-#include "Util.h"
-#include <vector>
-#include <iostream>
 #include <string>
-#include <fstream>
-#include <random>
-#include <chrono>
+
+
 
 int main() {
 	int pop_size = 50;
@@ -17,8 +13,7 @@ int main() {
 	int max_stale_iter = 500;
 	int max_generations = 20000;
 
-    std::string data_path = "./Data/bcl380.tsp";
-	//std::string data_path = "./Data/xqf131.tsp";
+    std::string data_path = "./Data/icw1483.tsp";
 
 	Evolution ev1(data_path, pop_size);
 	ev1.Evolve(prob, number_of_pairs, max_stale_iter, max_generations, true);
